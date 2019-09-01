88c066370f20dfbfe8525211c5d494036a78ba87
Lynux Ansay <lynux_ansay@dlsu.edu.ph>
2019-09-02 00:52:52 +0800

    offline files for cdn

--- app.js
+++ app.js
@@ -68,6 +68,7 @@ app.use((req,res,next) =>{
 })
 
 
+
 // Routes
 app.use(express.static(__dirname + '/public'));
 app.use('/register', require('./routes/registration'))
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 
+router.get('/accounting', (req,res) =>
+    res.render('accounting')
+)
 
+router.get('/members', (req,res) =>
+    res.render('members')
+)
 
 
 
