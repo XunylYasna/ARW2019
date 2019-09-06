d278171292378a0d9495c8b3f42debfd6064b2d0
rebecabecalyn <45415030+rebecabecalyn@users.noreply.github.com>
2019-09-07 01:48:26 +0800

    member db-ish

--- routes/registration.js
+++ routes/registration.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-
 router.get('/', (req,res) =>
     res.render('register')
 )
 
+router.post('/process', urlencoder, function(req, res){
+    var name = req.body.name;
+    var idNum = req.body.idNum;
+    var email = req.body.email;
+    var course = req.body.course;
+    var contactNum = req.body.contactNum;
+    var facebookName = req.body.facebookName;
 
+    let member = new Member({
+        name: name,
+        idNum: idNum,
+        email: email,
+        course: course,
+        contactNum: contactNum,
+        facebookName: facebookName
+    });
+})
 
 
 module.exports = router;
