6edd809cb9241143ee8d1a55c947f51559e91479
unknown <ma_patricia_villaroman@dlsu.edu.ph>
2019-09-08 23:26:11 +0800

    changed routes/registration.js to fit register.hbs

--- routes/registration.js
+++ routes/registration.js
@@ -5,21 +5,32 @@ router.get('/', (req,res) =>
     res.render('register')
 )
 
-router.post('/process', urlencoder, function(req, res){
-    var name = req.body.name;
-    var idNum = req.body.idNum;
-    var email = req.body.email;
-    var course = req.body.course;
-    var contactNum = req.body.contactNum;
-    var facebookName = req.body.facebookName;
+router.post('/process', /*urlencoder,*/ function(req, res){
+    let idNum = req.body.id_number;
+    let firstName = req.body.first_name;
+    let middleName = req.body.middle_name;
+    let lastName = req.body.last_name;
+    let email = req.body.dlsu_mail;
+    let course = req.body.course;
+    let contactNum = req.body.contact_number;
+    let facebookName = req.body.facebook_name;
+    let isOfficer = req.body.is_officer;
+    let officerPos = req.body.officer_position;
+    let recieptNum = req.body.reciept_number;
+
 
     let member = new Member({
-        name: name,
+        firstName: firstName,
+        middleName: middleName,
+        lastName: lastName,
         idNum: idNum,
         email: email,
         course: course,
         contactNum: contactNum,
-        facebookName: facebookName
+        facebookName: facebookName,
+        isOfficer: isOfficer,
+        officerPos: isOfficer,
+        recieptNum: recieptNum
     });
 })
 
