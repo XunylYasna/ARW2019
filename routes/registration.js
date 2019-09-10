a88932ffd6a7a58341b35aea7b3ddcc16de00c86
unknown <ma_patricia_villaroman@dlsu.edu.ph>
2019-09-10 22:37:53 +0800

    added routes

--- routes/registration.js
+++ routes/registration.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const bodyparser = require("body-parser");
+const urlencoder = bodyparser.urlencoded({
+    extended : true
+});
+router.use(urlencoder);
+const Member = require("../models/Member");
 
 router.get('/', (req,res) =>
     res.render('register')
 )
 
-router.post('/process', /*urlencoder,*/ function(req, res){
+router.get(['/', '/honorary', '/old', '/new','/old/1', '/old/2', '/old/3', '/old/4', '/old/5', 
+    '/new/1', '/new/2', '/new/3', '/new/4', '/new/5'], (req,res) =>
+    res.render('register')
+);
+
+router.post('/process', urlencoder, function(req, res){
+    console.log('/process');
     let idNum = req.body.id_number;
     let firstName = req.body.first_name;
     let middleName = req.body.middle_name;
@@ -14,25 +26,9 @@ router.post('/process', /*urlencoder,*/ function(req, res){
     let course = req.body.course;
     let contactNum = req.body.contact_number;
     let facebookName = req.body.facebook_name;
-    let isOfficer = req.body.is_officer;
-    let officerPos = req.body.officer_position;
-    let recieptNum = req.body.reciept_number;
-
-
-    let member = new Member({
-        firstName: firstName,
-        middleName: middleName,
-        lastName: lastName,
-        idNum: idNum,
-        email: email,
-        course: course,
-        contactNum: contactNum,
-        facebookName: facebookName,
-        isOfficer: isOfficer,
-        officerPos: isOfficer,
-        recieptNum: recieptNum
-    });
-})
+    let memberType = req.body.memberType;
+    let receiptNum = req.body.receipt_number;
+});
 
 
 module.exports = router;
