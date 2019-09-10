7b82d2921143ef9647126f6e92c1f022dc415dc2
unknown <ma_patricia_villaroman@dlsu.edu.ph>
2019-09-10 22:27:06 +0800

    modified Member model

--- models/Member.js
+++ models/Member.js
@@ -1,33 +1,62 @@
 const mongoose = require('mongoose');
 
 const MemberSchema = new mongoose.Schema({
-    name: {
+    idNum: {
+        type: String,
+        required: true
+    }, 
+    firstName: {
+        type: String,
+        required: true
+    }, 
+    middleName: {
+        type: String,
+        required: true
+    }, 
+    lastName: {
+        type: String,
+        required: true
+    }, 
+    email: {
+        type: String,
+        required: true
+    }, 
+    course: {
+        type: String,
+        required: true
+    }, 
+    contactNum: {
+        type: Number
+    }, 
+    fbName: {
+        type: String
+    }, 
+    memberType: {
+        type: String,
+        required: true
+    }, 
+    receiptNum: {
         type: String,
         required: true
     },
-
-    idNum: {
-        type: Number,
-        required: true,
-        unique: true
-    },
-
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-
     datejoined: {
         type: Date,
         default: Date.now
-    },
+    }
+});
 
-    course:String,
-    contactNum: Number,
-    facebookName: String
-})
+const Member = mongoose.model('Member', MemberSchema);
 
-const Member = mongoose.model('Member', MemberSchema)
+exports.create = function(member){
+    return new Promise(function(resolve, reject){
+      let m = new Member(member)
+  
+      m.save().then((newMember)=>{
+        resolve(newMember)
+      }, (err)=>{
+        reject(err)
+      })
+    })
+};
 
 module.exports = Member;
