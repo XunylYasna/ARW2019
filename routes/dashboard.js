c6a78a752cd48ebe0189c28422227383416496ed
unknown <silentcommentator1216@gmail.com>
2019-09-28 01:45:28 +0800

    database server added

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -7,6 +7,18 @@ const router = express.Router();
 // var db = new PouchDB('http://localhost:5984/members'); // web
 // db.info()
 
+// database
+const PouchDB = require('pouchdb');
+const db = new PouchDB('http://localhost:5984/members');
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../lscs-arw-firebase-adminsdk-vdtdd-2c1a8fc18a.json");
+
+var firebase = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://lscs-arw.firebaseio.com"
+});
+
 router.get('/accounting', (req,res) =>
     res.render('accounting')
 )
@@ -15,7 +27,30 @@ router.get('/members', (req,res) =>
     res.render('members')
 )
 
-
+router.get('/sync', (req,res) => {
+    // if ()
+    db.allDocs({
+        include_docs: true
+    }).then(function(result) {
+        db.allDocs({
+            include_docs: true
+        }).then(function(result) {
+            result.rows.forEach(function (row) {
+                var doc = row.doc;
+                delete doc._rev;
+                firebase.database().ref('/members/' + doc._id).set(doc)
+            });
+            console.log("Syncing Done!");
+        }).catch(function (err) {
+            console.log(err);
+        });
+        
+        console.log("Syncing Done!");
+        res.redirect('/dashboard/members');
+    }).catch(function (err) {
+        console.log(err);
+    });
+})
 
 
 module.exports = router;
